6f77612ae4e02acd27da53e4e7659647db196d29
JSaavedraHwl <saavedra190202@outlook.com>
2024-04-17 23:32:54 -0400

    correcion tamaño items carrito

--- balones.js
+++ balones.js
@@ -55,7 +55,7 @@ function armarTarjeta(item) {
 }
 function elementoCarrito(item) {
     return `
-    <div class="col-md-8">
+    <div class="col-md-12">
         <div class="card mb-3">
             <div class="row g-0">
                 <div class="col-md-4">
